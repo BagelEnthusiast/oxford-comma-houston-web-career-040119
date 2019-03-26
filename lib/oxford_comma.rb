d50c108c7dea1ba971a0933cbe75c4e4c08ffdc1
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-26 01:59:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
     "#{array[0]}, #{array[1]}, and #{array[2]}"
   else
     #length = array.length
-    "#{array[0...-1].join(", ")} and #{array[-1]}"
+    "#{array[0...-1].join(", ")}, and #{array[-1]}"
   end   
 end
 
