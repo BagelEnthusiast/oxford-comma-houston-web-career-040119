ce161a3b1b84a89be7fd8cfd33f01819c429ecc1
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-26 01:22:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
     array.join
-  elseif array.length == 2
+  elsif array.length == 2
     array.join(" and ")
   #else if array.length = 3
   end   
 end
-end 
